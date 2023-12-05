ffc70f38ff78d0ee955798ffd141e07a2b318f31
{eunnnnnnn} <pje9978@gmail.com>
2023-12-05 23:48:22 +0900

    Style: 컴포넌트 스타일 변경

--- src/components/subpage/result.jsx
+++ src/components/subpage/result.jsx
@@ -1,12 +1,12 @@
-import { FaCheckCircle } from "react-icons/fa";
+import { IoIosCheckmarkCircleOutline } from "react-icons/io";
 
 function Result({result}) {
     return ( <>
         <section className="mt-6">
             {result.map((item, index) => (
                 <>
-                    <div key={index} role="group" aria-label="result" className="container mx-auto flex items-ceter justify-center text-left ">
-                        <FaCheckCircle  className="mr-3 flex self-center items-center" />
+                    <div key={index} role="group" aria-label="result" className="container mx-auto flex items-ceter justify-center text-left hover:text-gray-200">
+                        <IoIosCheckmarkCircleOutline className="mr-3 flex self-center items-center" />
                         <h4>{item}</h4>
                     
                     </div>
