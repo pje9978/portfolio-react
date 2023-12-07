347e403197c890048d91b2a3dedd7a2d31b19466
{eunnnnnnn} <pje9978@gmail.com>
2023-12-07 23:03:28 +0900

    Fix: link 오류 수정

--- src/components/work/urlNav.jsx
+++ src/components/work/urlNav.jsx
@@ -1,14 +1,16 @@
 import { Link } from "react-router-dom";
 
 
+
 function UrlNav({id, index, url, link, item}) {
-        
+
     return (<>
         <article className="bg-[#202020] w-full flex flex-row p-4 rounded-md md:flex-wrap  justify-center items-center truncate">
             <h2 className="hidden">page url</h2>
             <section className="urlArea rounded inline-flex overflow-hidden flex-wrap text-black/70">
                 <h2 className="hidden">urlArea</h2>
                 <ul className="flex">
+                    
                     {url && url.map((urlItem, index) => (
                         <li key={index}><a className="block text-sm bg-white px-3 py-1 capitalize" href={urlItem === undefined ? "#" : urlItem}>{urlItem === undefined ? "" : `page${index+1}`}</a></li>
                     ))} 
@@ -17,19 +19,18 @@ function UrlNav({id, index, url, link, item}) {
         
             <section className="linkArea rounded inline-flex overflow-hidden flex-wrap ">
                 <h2 className="hidden">linkArea</h2>
-                <ul className="flex">
-                    {link && Object.keys(link).map((a,index)=>(
-                        <>
-                            <li key={id}>
-                                <a className="block text-sm bg-black/60 hover:bg-black/100 hover: hidden xl:flex px-4 py-1 capitalize " href={link === undefined ? "#" : link[a]}>{link === undefined ? "" : a}</a>
-                            </li>
-                            <li key={id}>
-                                <Link to={`pages/${id}`} className="block text-sm bg-black/60 hover:bg-black/100 px-4 py-1 capitalize">자세히 보기</Link>
-                            </li>
-                        </>
-                    ))}
-               
-                </ul>
+                    {Object.keys(link).map((key,index)=>{
+                        const value = link[key];
+                        if(value === null){
+                            return null;
+                        }else{
+                            return(
+                                <a key={index} className="block text-sm bg-black/60 hover:bg-black/100 px-4 py-1 capitalize" href={value}>{key}</a>
+                            );
+                        };
+                     })}
+
+                    <Link to={`pages/${id}`} className="block text-sm bg-black/60 hover:bg-black/100 px-4 py-1 capitalize">자세히 보기</Link>
             </section>
 
         </article>
