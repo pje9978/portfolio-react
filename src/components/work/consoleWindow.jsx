4f3e56ec250b59155ee80c0536612b26d0234142
{eunnnnnnn} <pje9978@gmail.com>
2023-12-11 14:33:23 +0900

    Fix: 컴포넌트 오류 수정 - language 키의 값이 ture 경우만 가져오도록 수정

--- src/components/work/consoleWindow.jsx
+++ src/components/work/consoleWindow.jsx
@@ -1,4 +1,6 @@
 function ConsoleWindow({id,title, language, part, percent}) {
+    const keywords = ["html", "css", "scss", "tailwindcss", "javascript", "react", "notion", "photoshop", "illustrator", "GitHub","firebase","figma"];
+ 
     return ( <>
         <section key={id} className="work h-[200px] relative  overflow-y-scroll flex flex-col w-full p-4  items-start font-[monospace] text-sm overflow-y-auto overflow-x-hidden">
             <h2 className="hidden">work</h2>
@@ -7,9 +9,16 @@ function ConsoleWindow({id,title, language, part, percent}) {
             
             <h3 className="before:content-['>'] before:mr-2 opacity-50">language</h3>
             <ul className="flex before:content-['>'] before:mr-2 flex-wrap" >
-                {Object.keys(language).map((lang,index) => (
-                    <li key={index} className="mr-2 pacity-100 "><h4>{lang}</h4></li>
-                ))}
+                {keywords.map((keyword,index) => {
+                    if(language[keyword] === true){
+                    return (
+                        <li key={index} className="mr-2 pacity-100 "><h4>{keyword}</h4></li>
+                    );
+                    }else {
+                        return null;
+                    }
+                })}
+
             </ul>
             <h3 className="before:content-['>'] before:mr-2 opacity-50">part</h3>
             <ul className="flex before:content-['>'] before:mr-2 " >
